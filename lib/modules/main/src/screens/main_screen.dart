6077550a9cc56170822b52b4ba33a498cbf23da0
qkrgorn3617 <qkrgorn3617@naver.com>
2025-03-06 22:00:54 +0900

    appbar

--- lib/modules/main/src/screens/main_screen.dart
+++ lib/modules/main/src/screens/main_screen.dart
@@ -8,32 +8,30 @@ class MainScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
-        toolbarHeight: 80,
+        toolbarHeight: MediaQuery.of(context).size.width * 0.13,
         leading: IconButton(
           onPressed: () {
             debugPrint("메뉴 클릭했습니다.");
           },
-          icon: Icon(Icons.menu, size: 38, color: Colors.white),
-          padding: EdgeInsets.only(left: 20),
+          icon: Icon(Icons.menu, size: 28, color: Colors.white),
+          // margin: EdgeInsets.only(left: 10),
         ),
         actions: [
           Padding(
             padding: EdgeInsets.only(right: 15),
-            child: GestureDetector(
+            child: InkWell(
               onTap: () {
                 debugPrint("프로필 클릭");
               },
+              borderRadius: BorderRadius.circular(50),
               child: Container(
-                width: 55,
-                height: 55,
-                margin: EdgeInsets.only(right: 15),
+                width: 43,
+                height: 43,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(50),
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.black.withAlpha(
-                        (0.3 * 255).toInt(),
-                      ), // 그림자 색상
+                      color: Colors.black.withAlpha((0.3 * 255).toInt()),
                       blurRadius: 1,
                       spreadRadius: 1,
                       offset: Offset(1.5, 1.5),
