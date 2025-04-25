f46b8cc9b94fdd1e2e293b047a0cbd45289181d1
ParkJunBeom <pjb3779@naver.com>
2025-04-25 16:38:38 +0800

    enlv1s2c3gn3,gn4

--- lib/level_1/2_3/level_1_2_3_think3.dart
+++ lib/level_1/2_3/level_1_2_3_think3.dart
@@ -133,93 +133,290 @@ class _LevelOneTwoThreeThink3State extends State<LevelOneTwoThreeThink3> {
   // 정답 체크하는 함수. 정답 체크로직 구현 필요.
   void checkAnswer() {}
 
+  int? selectedIndex;
+
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(title: Text('빠진 수 찾기')),
       body:
-          isLoading
-              ? const Center(child: EnProblemSplashScreen())
-              : Padding(
-                padding: const EdgeInsets.all(16.0),
-                child:
-                    numberList.isNotEmpty
-                        ? Column(
-                          children: [
-                            SizedBox(height: 16),
-                            HeaderWidget(headerText: '개념학습활동'),
-                            SizedBox(height: 16),
-                            QuestionTextWidget(
-                              questionText:
-                                  '숫자가 들어갈 알맞은 위치를 찾아 <보기> 와 같이 O표 하세요.',
-                            ),
-                            SizedBox(height: 15),
-                            ExampleContainer(),
-                            SizedBox(height: 30),
-                            Container(
-                              width: 600,
-                              height: 300,
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.amber,
-                                  width: 2,
-                                ),
-                              ),
-                              child: Column(
-                                children: [
-                                  SizedBox(height: 46),
-                                  ClickableContainer(numberList: numberList),
-                                  SizedBox(height: 120),
-                                  Container(
-                                    alignment: Alignment.center,
-                                    width: 100,
-                                    height: 50,
+      isLoading
+          ? const Center(child: EnProblemSplashScreen())
+          : Padding(
+        padding: const EdgeInsets.all(16.0),
+        child:
+        numberList.isNotEmpty
+            ? Column(
+          children: [
+            SizedBox(height: 16),
+            HeaderWidget(headerText: '개념학습활동'),
+            SizedBox(height: 16),
+            QuestionTextWidget(
+              questionText:
+              '숫자가 들어갈 알맞은 위치를 찾아 <보기> 와 같이 O표 하세요.',
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                      height: screenHeight * 0.3,
+                      width: screenWidth * 0.85,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.orangeAccent,
+                          width: 4,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                              width: screenWidth * 0.75,
+                              height: screenHeight * 0.1,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: List.generate(5, (index) {
+                                  final contents = ['1', '', '3', '○', '5'];//데이터 넣기
+
+                                  return Container(
+                                    height: screenHeight * 0.06,
+                                    width:  screenWidth * 0.15,
                                     decoration: BoxDecoration(
-                                      color: Colors.amber.shade200,
-                                      border: Border.all(
-                                        width: 1,
-                                        color: Colors.black,
-                                      ),
+                                      color: Color(0xFFFef1c4),
+                                      border: Border.all(color: Color(0xFF9c6a17)),
                                     ),
-                                    child: Text(
-                                      '$givenNumber',
-                                      style: TextStyle(fontSize: 25),
+                                    child: Center(
+                                        child: contents[index] == '○'
+                                            ? Container(
+                                          width: 40,
+                                          height: 40,
+                                          decoration: BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              border: Border.all(
+                                                color: Colors.black,
+                                                width: 2,
+                                              )
+                                          ),
+                                        )
+                                            :
+                                        Text(
+                                          contents[index],
+                                          style: TextStyle(
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        )
+                                    ),
+                                  );
+                                }),
+                              )
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 0),
+                            child: Image.asset(
+                              'assets/images/logo1.png', //화살표 구현
+                              width: screenWidth * 0.3,
+                              height: screenHeight * 0.06,
+                            ),
+                          ),
+                          SizedBox(
+                              child: Container(
+                                height: screenHeight * 0.06,
+                                width:  screenWidth * 0.15,
+                                margin: EdgeInsets.symmetric(horizontal: 2),
+                                decoration: BoxDecoration(
+                                  color: Color(0xFFFef1c4),
+                                  border: Border.all(color: Color(0xFF9c6a17)),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    'Num',
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
-                                ],
+                                ),
+                              )
+                          ),
+                        ],
+                      )
+                  ),
+                  Positioned(
+                    top: 0,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.orangeAccent,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 3,
+                          ),
+                        ],
+                      ),
+                      child: const Text(
+                        "<보기>",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.05),
+            Column(
+              children: [
+                SizedBox(
+                  width: screenWidth * 0.75,
+                  height: screenHeight * 0.1,
+                  child: GridView.count(
+                    crossAxisCount: 5,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    padding: EdgeInsets.zero,
+                    childAspectRatio: 1.5,
+                    children: List.generate(5, (index) {
+                      final contents2 = ['5', null, '7', null, '8']; // 예시용
+                      final isSelectable = contents2[index] == null;
+                      final isSelected = selectedIndex == index;
+
+                      return Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          SizedBox(
+                            width: screenWidth * 0.15,
+                            height: screenHeight * 0.1,
+                            child: ElevatedButton(
+                              onPressed: isSelectable
+                                  ? () {
+                                setState(() {
+                                  selectedIndex = index;
+                                });
+                              }
+                                  : null,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFFFef1c4),
+                                foregroundColor: Colors.black,
+                                elevation: 3,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.zero,
+                                  side: const BorderSide(color: Color(0xFF9c6a17)),
+                                ),
+                                padding: const EdgeInsets.all(5.0),
+                                disabledBackgroundColor: const Color(0xFFFef1c4),
+                                disabledForegroundColor: Colors.black,
+                              ),
+                              child: contents2[index] != null
+                                  ? Text(
+                                '$givenNumber',
+                                style: const TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                                  : const SizedBox.shrink(),
+                            ),
+                          ),
+
+                          if (isSelected)
+                            Positioned(
+                              child: IgnorePointer(
+                                child: Container(
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      color: Colors.black,
+                                      width: 2,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                            SizedBox(height: 40),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                ButtonWidget(
-                                  width: 120,
-                                  height: 60,
-                                  buttonText: '제출하기',
-                                  fontSize: 18,
-                                ),
-                                SizedBox(width: 10),
-                                ButtonWidget(
-                                  height: 60,
-                                  width: 120,
-                                  buttonText:
-                                      nextProblemCode == '' ? '학습 완료' : '다음 문제',
-                                  fontSize: 18,
-                                  onPressed: () {
-                                    var route =
-                                        nextProblemCode == ''
-                                            ? '/level1'
-                                            : '/level1/$nextProblemCode';
-                                    Modular.to.pushNamed(route);
-                                  },
-                                ),
-                              ],
-                            ),
-                          ],
-                        )
-                        : const Center(child: CircularProgressIndicator()),
-              ),
+                        ],
+                      );
+                    }),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 0),
+                  child: Image.asset(
+                    'assets/images/logo2.png', //화살표 구현하기
+                    width: screenWidth * 0.3,
+                    height: screenHeight * 0.06,
+                  ),
+                ),
+                SizedBox(
+                    child: Container(
+                      height: screenHeight * 0.06,
+                      width:  screenWidth * 0.15,
+                      margin: EdgeInsets.symmetric(horizontal: 2),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFFef1c4),
+                        border: Border.all(color: Color(0xFF9c6a17)),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Num',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    )
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ButtonWidget(
+                  width: 120,
+                  height: 60,
+                  buttonText: '제출하기',
+                  fontSize: 18,
+                ),
+                SizedBox(width: 10),
+                ButtonWidget(
+                  height: 60,
+                  width: 120,
+                  buttonText:
+                  nextProblemCode == '' ? '학습 완료' : '다음 문제',
+                  fontSize: 18,
+                  onPressed: () {
+                    var route =
+                    nextProblemCode == ''
+                        ? '/level1'
+                        : '/level1/$nextProblemCode';
+                    Modular.to.pushNamed(route);
+                  },
+                ),
+              ],
+            ),
+          ],
+        )
+            : const Center(child: CircularProgressIndicator()),
+      ),
     );
   }
 }
