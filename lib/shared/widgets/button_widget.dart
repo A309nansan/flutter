8415e1ed999607e7c21029ee99e029df29b7a1d1
qkrgorn3617 <qkrgorn3617@naver.com>
2025-06-20 23:02:49 +0900

    [FE/REFACTOR] EN Level1 1과 1차시 주요학습활동 UI 리팩토링

--- lib/shared/widgets/button_widget.dart
+++ lib/shared/widgets/button_widget.dart
@@ -1,31 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-/// 재사용 버튼 위젯
-///
-/// 기본 사용 예시:
-/// ```dart
-/// ButtonWidget(
-///   height: 50,                          // 필수
-///   width: 200,                          // 필수
-///   buttonText: '제출하기',             // 필수
-///   fontSize: 16,                        // 필수
-///   backgroundColor: Colors.red,        // (선택) 버튼 배경색
-///   textColor: Color(0xFFE1E1FF),       // (선택) 텍스트 색상
-///   borderRadius: 30.0,                 // (선택) 버튼 둥글기
-///
-///   onPressed: () async {               // (선택) 클릭 시 비동기 처리
-///     await Future.delayed(Duration(seconds: 2));
-///     print('제출 완료!');
-///   },
-///
-///   또는
-///
-///   onPressed: () {                     // (선택) 클릭 시 실행할 동기 함수
-///     print('제출 완료!');
-///   },
-/// ),
-/// ```
 class ButtonWidget extends StatefulWidget {
   final double height;
   final double width;
@@ -88,7 +63,7 @@ class _AsyncButtonWidgetState extends State<ButtonWidget> {
           foregroundColor: Colors.black,
           disabledBackgroundColor:
               widget.backgroundColor ?? const Color(0xFFFFFAE1),
-
+          padding: EdgeInsets.zero,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(widget.borderRadius ?? 30.0),
           ),
